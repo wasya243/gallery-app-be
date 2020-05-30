53a640673b71fec661a183dc063cf7bdc983867a
vkharchenko <vkharchenko@vkharchenkosMBP.netis>
2020-05-30 22:41:39 +0300

    First commit.

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(`server is listening on ${port}`);
+  }
+});
