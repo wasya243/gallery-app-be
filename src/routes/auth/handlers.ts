c7f21b5c0eb9c821565fa51b2194146d17200283
vkharchenko <vkharchenko@vkharchenkosMBP.netis>
2020-06-03 20:19:55 +0300

    Add sign in method.

--- src/routes/auth/handlers.ts
+++ src/routes/auth/handlers.ts
@@ -2,31 +2,67 @@ import express from 'express';
 
 import { DatabaseManager } from '../../db/database-manager';
 import { GalleryUser } from '../../db/models';
-import { encryptPassword } from '../../auth/crypt';
+import { encryptPassword, verifyPassword } from '../../auth/crypt';
+import { sign } from '../../auth/token';
+
+export async function signIn(req: express.Request, res: express.Response, next: express.NextFunction) {
+  const { password, email } = req.body;
+
+  try {
+    const connection = DatabaseManager.getConnection();
+    const galleryUserRepository = connection.getRepository(GalleryUser);
+
+    const galleryUser = await galleryUserRepository.findOne({ email });
+    if (!galleryUser || !(await verifyPassword(password, galleryUser.password))) {
+      console.error(`user not found or invalid password`);
+    }
+
+    const galleryUserPayload = { id: galleryUser.id };
+    const accessToken = await sign(galleryUserPayload);
+    const response = {
+      accessToken,
+      galleryUser: {
+        email: galleryUser.email,
+        firstName: galleryUser.firstName,
+        lastName: galleryUser.lastName
+      }
+    };
+
+    res.send(response);
+  } catch (error) {
+    // TODO: add error handler
+    console.error(error);
+  }
+}
 
 export async function signUp(req: express.Request, res: express.Response, next: express.NextFunction) {
   const { firstName, lastName, password, email } = req.body;
 
   try {
     const connection = DatabaseManager.getConnection();
-    const userRepository = connection.getRepository(GalleryUser);
+    const galleryUserRepository = connection.getRepository(GalleryUser);
 
-    const user = await userRepository.findOne({ email });
+    const user = await galleryUserRepository.findOne({ email });
 
     if (user) {
       console.error(`user already exists`);
     }
 
-    const userToCreate = new GalleryUser();
-    userToCreate.firstName = firstName;
-    userToCreate.lastName = lastName;
-    userToCreate.email = email;
-    userToCreate.password = await encryptPassword(password);
+    const galleryUserToCreate = new GalleryUser();
+    galleryUserToCreate.firstName = firstName;
+    galleryUserToCreate.lastName = lastName;
+    galleryUserToCreate.email = email;
+    galleryUserToCreate.password = await encryptPassword(password);
 
-    await userRepository.save(userToCreate);
+    await galleryUserRepository.save(galleryUserToCreate);
 
-    // TODO: think of response (do not return password and id --> use projection)
-    res.send(userToCreate);
+    const response = {
+      firstName: galleryUserToCreate.firstName,
+      lastName: galleryUserToCreate.lastName,
+      email: galleryUserToCreate.email
+    };
+
+    res.send(response);
   } catch (error) {
     // TODO: add error handler
     console.error(error);
--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 
-import { signUp } from './handlers';
+import { signUp, signIn } from './handlers';
 
 export const router = express.Router();
 
 // TODO: add joi validation schema
 router.post('/auth/sign-up', signUp);
+
+// TODO: add joi validation schema
+router.post('/auth/sign-in', signIn);
