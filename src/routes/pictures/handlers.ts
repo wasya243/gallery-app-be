5a555dd962ea2bb2c798f3fc4c9def1d5dfcf2c2
vkharchenko <vkharchenko@vkharchenkosMBP.netis>
2020-06-13 16:10:41 +0300

    Add one more handler to delete picture.

--- src/routes/pictures/handlers.ts
+++ src/routes/pictures/handlers.ts
@@ -2,6 +2,7 @@ import express from 'express';
 
 import { DatabaseManager } from '../../db/database-manager';
 import { Picture } from '../../db/models/Picture';
+import httpErrors from 'http-errors';
 
 export async function getGalleryUserPictures(req: express.Request, res: express.Response, next: express.NextFunction) {
   // TODO: try extending request type
@@ -27,10 +28,30 @@ export async function getGalleryUserPictures(req: express.Request, res: express.
 //   }
 // }
 //
-// export async function deletePicture(req: express.Request, res: express.Response, next: express.NextFunction) {
-//   try {
-//
-//   } catch (err) {
-//
-//   }
-// }
+
+export async function deletePicture(req: express.Request, res: express.Response, next: express.NextFunction) {
+  // TODO: try extending request type
+  // @ts-ignore
+  const galleryUserId = req.userData.id;
+  const pictureId = parseInt(req.params.id, 10);
+  try {
+    const connection = DatabaseManager.getConnection();
+    const pictureRepository = connection.getRepository(Picture);
+
+    const pictureToDelete = await pictureRepository.findOne(pictureId);
+
+    if (!pictureToDelete) {
+      return next(httpErrors(404, `Picture with given id ${pictureId} is not found`));
+    }
+
+    if (pictureToDelete && pictureToDelete.galleryUser.id !== galleryUserId) {
+      return next(httpErrors(400, `Picture with given id ${pictureId} is not found does not belong to user with id ${galleryUserId}`));
+    }
+
+    await pictureRepository.remove(pictureToDelete);
+
+    res.status(204).end();
+  } catch (err) {
+    next(err);
+  }
+}
--- src/routes/pictures/index.ts
+++ src/routes/pictures/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 
 import { authMiddleware } from '../../auth/middlewares/auth';
-import { getGalleryUserPictures } from './handlers';
+import { getGalleryUserPictures, deletePicture } from './handlers';
 
 export const router = express.Router();
 
 router.get('/user-pictures', authMiddleware, getGalleryUserPictures);
+
+router.delete('/pictures/:id', authMiddleware, deletePicture);
